4ee74f4eea8e281e1cce1b4ada4f61c3759b1211
Franco Itria <francoitria01@gmail.com>
2023-06-08 03:02:08 -0300

    Footer

--- Front/src/components/footer/footer.jsx
+++ Front/src/components/footer/footer.jsx
@@ -1,4 +1,25 @@
+import style from "./footer.module.css";
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
+import { SiGmail } from "react-icons/si";
+import { FaLinkedinIn } from "react-icons/fa";
 
-export default function Footer(){
-
+export default function Footer() {
+  return (
+    <footer className={style.nav}>
+      <h1 className={style.name}>
+      Franco Itria
+      </h1>
+      <div className={style.elements}>
+        <a href="https://github.com/fran-itria">
+          <IoLogoGithub size={40} className={style.github}/>
+        </a>
+        <a href="https://www.linkedin.com/in/francoitria/">
+          <FaLinkedinIn size={40} className={style.linkedin}/>
+        </a>
+        <a href="mailto:francoitria01@gmail.com">
+          <SiGmail size={40} className={style.gmail}/>
+        </a>
+      </div>
+    </footer>
+  );
 }
