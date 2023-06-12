8ed8ba030d8322a01c1576a5c19fbecb4e51e736
Franco Itria <francoitria01@gmail.com>
2023-06-12 14:07:10 -0300

    Desactivado método de contacto

--- Front/src/components/contact/contact.jsx
+++ Front/src/components/contact/contact.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import style from "./contact.module.css";
 import axios from "axios";
-import { MdKeyboardDoubleArrowUp } from 'react-icons/md'
+import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 
 export default function Contact() {
   const [inputs, setInputs] = useState({
@@ -9,8 +9,9 @@ export default function Contact() {
     email: "",
     message: "",
   });
-  const [errors, setErrors] = useState({});
-  
+  const [errors, setErrors] = useState();
+  const URL_BASE = "https://back-portafolio.vercel.app" || "http://localhost:3001";
+
   const validate = (inputs, setErrors) => {
     let error = {};
     const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{3}$/i;
@@ -18,35 +19,33 @@ export default function Contact() {
     if (inputs.name.length == 0) {
       error.name = "Campo obligatorio";
     } else delete error.name;
-    
+
     if (inputs.email.length == 0) {
       error.email = "Campo obligatorio";
     } else if (!regexEmail.test(inputs.email)) {
       error.email = "Debe ser un correo válido";
     } else delete error.email;
-    
+
     if (inputs.message.length == 0) {
       error.message = "Campo obligatorio";
     } else delete error.message;
-    
+
     setErrors(error);
   };
-  
+
   const handleInput = (event) => {
     const { name, value } = event.target;
     setInputs({ ...inputs, [name]: value });
-    validate({ ...inputs, [name]: value }, setErrors, errors);
+    validate({ ...inputs, [name]: value }, setErrors);
   };
+
+  useEffect(() => validate(inputs, setErrors), []);
   
-  useEffect(() => validate(inputs, setErrors, errors), []);
-  
-  const URL_BASE = "https://back-portafolio.vercel.app"
-  // http://localhost:3001
   const submit = async (event) => {
     event.preventDefault();
-    const response = await axios.post(`${URL_BASE}/contact`, inputs);
-    console.log(response);
-    if (response.status == 200) window.alert("Mensaje enviado exitosamente");
+    window.alert('Método para contactarte no implementado aún')
+    // const response = await axios.post(`${URL_BASE}/contact`, inputs);
+    // if (response.status == 200) window.alert("Mensaje enviado exitosamente");
   };
 
   return (
@@ -62,9 +61,9 @@ export default function Contact() {
               type="text"
               name="name"
               onChange={(event) => handleInput(event)}
-              className={errors.name ? style.inputError : style.input}
+              className={errors && errors.name ? style.inputError : style.input}
             ></input>
-            {errors.name ? (
+            {errors && errors.name ? (
               <div className={style.error}>
                 <b> * {errors.name} </b>
               </div>
@@ -78,10 +77,12 @@ export default function Contact() {
               type="email"
               name="email"
               onChange={(event) => handleInput(event)}
-              className={errors.email ? style.inputError : style.input}
+              className={
+                errors && errors.email ? style.inputError : style.input
+              }
               placeholder="ejemplo@gmail.com"
             ></input>
-            {errors.email ? (
+            {errors && errors.email ? (
               <div className={style.error}>
                 <b> * {errors.email} </b>
               </div>
@@ -96,9 +97,9 @@ export default function Contact() {
             className={errors && errors.message ? style.areaError : style.area}
             name="message"
             onChange={(event) => handleInput(event)}
-            spellCheck='true'
+            spellCheck="true"
           ></textarea>
-          {errors.message ? (
+          {errors && errors.message ? (
             <div className={style.errorMensaje}>
               <b> * {errors.message} </b>
             </div>
@@ -107,7 +108,7 @@ export default function Contact() {
           )}
         </div>
         <div className={style.button}>
-          {Object.keys(errors).length > 0 ? (
+          {errors && Object.keys(errors).length > 0 ? (
             <button type="submit" disabled className={style.enviar}>
               Enviar mensaje
             </button>
@@ -117,7 +118,7 @@ export default function Contact() {
             </button>
           )}
         </div>
-      {/* <a href="#about">
+        {/* <a href="#about">
         <MdKeyboardDoubleArrowUp size={70} color="white" className={style.top}/>
       </a> */}
       </form>
