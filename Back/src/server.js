26d678a09853a3b99eb47f0a2e396a2dde934c64
Franco Itria <francoitria01@gmail.com>
2023-09-06 21:45:58 -0300

    Using manual cors config

--- Back/src/server.js
+++ Back/src/server.js
@@ -4,14 +4,25 @@ const transporter = require("./index");
 const server = express();
 const PORT = process.env.PORT;
 
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
 server.use(express.json());
-server.use(
-  cors()
-);
-server.options(
-  "*",
-  cors()
-);
 
 server.post("/contact", async (req, res) => {
   const { email, name, message } = req.body;
