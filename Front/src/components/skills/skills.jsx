c9260c923673bde3ebe1ee88256b3558901998eb
Franco Itria <francoitria01@gmail.com>
2024-02-02 03:29:46 -0300

    skills responsive

--- Front/src/components/skills/skills.jsx
+++ Front/src/components/skills/skills.jsx
@@ -22,39 +22,39 @@ export default function Skills() {
         </div>
         <div className={style.skills}>
           <div>
-            <AiFillHtml5 color="#f06529" size={100} />
+            <AiFillHtml5 className={style.html} />
             <p> HTML </p>
           </div>
           <div>
-            <IoLogoCss3 color="#264de4" size={100} />
+            <IoLogoCss3 className={style.css} />
             <p> CSS </p>
           </div>
           <div>
-            <SiJavascript color="#f0db4f" size={100} />
+            <SiJavascript className={style.js} />
             <p> JavaScript </p>
           </div>
           <div>
-            <SiReact color="#7cc5d9" size={100} />
+            <SiReact className={style.react} />
             <p> React </p>
           </div>
           <div>
-            <SiNextdotjs size={100} />
+            <SiNextdotjs className={style.next} />
             <p> Next.js </p>
           </div>
           <div>
-            <IoLogoNodejs color="#3c873a" size={100} />
+            <IoLogoNodejs className={style.node} />
             <p> Node.js </p>
           </div>
           <div>
-            <SiSequelize color="#2379BD" size={100} />
+            <SiSequelize className={style.sql} />
             <p> Sequelize </p>
           </div>
           <div>
-            <SiTypescript color="#007acc" size={100} />
+            <SiTypescript className={style.ts} />
             <p> TypeScript </p>
           </div>
           <div>
-            <BsGit color="#f34f29" size={100} />
+            <BsGit className={style.git} />
             <p> Git </p>
           </div>
         </div>
