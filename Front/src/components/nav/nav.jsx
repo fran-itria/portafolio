c4a8119e3693dc5840e5dcfe92173754caf8d4b6
Franco Itria <francoitria01@gmail.com>
2024-02-02 01:52:19 -0300

    Added icon

--- Front/src/components/nav/nav.jsx
+++ Front/src/components/nav/nav.jsx
@@ -1,49 +1,63 @@
-import { useState } from "react";
+/* eslint-disable eqeqeq */
+// import { useState } from "react";
+import { FcAbout } from "react-icons/fc";
+import { FaBrain } from "react-icons/fa";
+import { FcBriefcase } from "react-icons/fc";
+import { FcBusinessContact } from "react-icons/fc";
 import style from "./nav.module.css";
 
 export default function Nav() {
-  const [active, setActive] = useState();
-  const handleActive = (event) => {
-    setActive(event.target.name);
-  };
+  // const [active, setActive] = useState();
+  // const handleActive = (event) => {
+  //   setActive(event.target.name);
+  // };
 
   return (
-      <nav className={style.nav}>
-        <h1 className={style.name}>Franco Itria</h1>
-        <div className={style.elements}>
-          <a
-            href="#about"
-            name="about"
-            className={active == "about" ? style.active : style.button}
-            onClick={(event) => handleActive(event)}
-          >
-            Sobre mi
-          </a>
-          <a
-            href="#skills"
-            name="skills"
-            className={active == "skills" ? style.active : style.button}
-            onClick={(event) => handleActive(event)}
-          >
-            Habilidades
-          </a>
-          <a
-            href="#proyects"
-            name="proyects"
-            className={active == "proyects" ? style.active : style.button}
-            onClick={(event) => handleActive(event)}
-          >
-            Proyectos
-          </a>
-          <a
-            href="#contact"
-            name="contact"
-            className={active == "contact" ? style.active : style.button}
-            onClick={(event) => handleActive(event)}
-          >
-            Contacto
-          </a>
-        </div>
-      </nav>
+    <nav className={style.nav}>
+      <h1 className={style.name}>Franco Itria</h1>
+      <div className={style.elements}>
+        <a
+          href="#about"
+          name="about"
+          className={style.button}
+          // onClick={(event) => handleActive(event)}
+        >
+          {/* <SiAboutdotme size={40} color="black" /> */}
+          <p> Sobre mí </p>
+          <FcAbout size={40} color="black" />
+          {/* Sobre mi */}
+        </a>
+        <a
+          href="#skills"
+          name="skills"
+          className={style.button}
+          // onClick={(event) => handleActive(event)}
+        >
+          <p> Habilidades </p>
+          <FaBrain size={34} />
+          {/* Habilidades */}
+        </a>
+        <a
+          href="#proyects"
+          name="proyects"
+          className={style.button}
+          // onClick={(event) => handleActive(event)}
+        >
+          <p>Proyectos </p>
+          <FcBriefcase size={40} color="lightblue" />
+          {/* Proyectos */}
+        </a>
+        <a
+          href="#contact"
+          name="contact"
+          className={style.button}
+          // onClick={(event) => handleActive(event)}
+        >
+          <p>Contacto </p>
+          <FcBusinessContact size={40} color="white" />
+          {/* Contacto */}
+        </a>
+      </div>
+    </nav>
   );
 }
