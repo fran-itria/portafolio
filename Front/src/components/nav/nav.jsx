0cd7421d199ee0899c9c325a443251442a5d00b4
Franco Itria <francoitria01@gmail.com>
2024-02-02 03:29:09 -0300

    Delete coments

--- Front/src/components/nav/nav.jsx
+++ Front/src/components/nav/nav.jsx
@@ -7,55 +7,26 @@ import { FcBusinessContact } from "react-icons/fc";
 import style from "./nav.module.css";
 
 export default function Nav() {
-  // const [active, setActive] = useState();
-  // const handleActive = (event) => {
-  //   setActive(event.target.name);
-  // };
-
   return (
     <nav className={style.nav}>
       <h1 className={style.name}>Franco Itria</h1>
       <div className={style.elements}>
-        <a
-          href="#about"
-          name="about"
-          className={style.button}
-          // onClick={(event) => handleActive(event)}
-        >
+        <a href="#about" name="about" className={style.button}>
           {/* <SiAboutdotme size={40} color="black" /> */}
-          <p> Sobre mí </p>
-          <FcAbout size={40} color="black" />
-          {/* Sobre mi */}
+          <p className={style.pNav}> Sobre mí </p>
+          <FcAbout className={style.icons} />
         </a>
-        <a
-          href="#skills"
-          name="skills"
-          className={style.button}
-          // onClick={(event) => handleActive(event)}
-        >
-          <p> Habilidades </p>
-          <FaBrain size={34} />
-          {/* Habilidades */}
+        <a href="#skills" name="skills" className={style.button}>
+          <p className={style.pNav}> Habilidades </p>
+          <FaBrain className={style.icons} />
         </a>
-        <a
-          href="#proyects"
-          name="proyects"
-          className={style.button}
-          // onClick={(event) => handleActive(event)}
-        >
-          <p>Proyectos </p>
-          <FcBriefcase size={40} color="lightblue" />
-          {/* Proyectos */}
+        <a href="#proyects" name="proyects" className={style.button}>
+          <p className={style.pNav}>Proyectos </p>
+          <FcBriefcase className={style.icons} />
         </a>
-        <a
-          href="#contact"
-          name="contact"
-          className={style.button}
-          // onClick={(event) => handleActive(event)}
-        >
-          <p>Contacto </p>
-          <FcBusinessContact size={40} color="white" />
-          {/* Contacto */}
+        <a href="#contact" name="contact" className={style.button}>
+          <p className={style.pNav}>Contacto </p>
+          <FcBusinessContact className={style.icons} />
         </a>
       </div>
     </nav>
