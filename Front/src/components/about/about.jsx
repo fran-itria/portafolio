99b3b3fa808828627d100e13cecd343701352a72
Franco Itria <francoitria01@gmail.com>
2024-04-09 01:27:27 -0300

    Changed presentation

--- Front/src/components/about/about.jsx
+++ Front/src/components/about/about.jsx
@@ -12,12 +12,13 @@ export default function About() {
         <p className={style.text}>
           Estoy encantado de tener la oportunidad de presentarme ante ustedes.
           <br></br>
-          Soy Franco Itria, tengo 21 años y recientemente completé un curso
-          intensivo de <b> Full Stack Developer </b> en Soy Henry. <br></br>
-          Durante el curso en Soy Henry, tuve la oportunidad de sumergirme en el
-          mundo del desarrollo web y adquirir una amplia gama de habilidades
-          técnicas. Aprendí sobre diferentes tecnologías y lenguajes de
-          programación como{" "}
+          Soy Franco Itria, tengo 22 años y en julio de 2023 completé con éxito
+          un curso intensivo de <b> Full Stack Developer </b>
+          en Soy Henry. <br></br>
+          Adquirí una sólida comprensión de las tecnologías más utilizadas en el
+          desarrollo web, desde el front-end hasta el back-end, tuve la
+          oportunidad de adquirir una amplia gama de habilidades técnicas.
+          Aprendí sobre diferentes tecnologías y lenguajes de programación como{" "}
           <b>
             {" "}
             HTML, CSS, JavaScript, Node.js, Express.js y bases de datos
@@ -25,10 +26,9 @@ export default function About() {
           También trabajé en proyectos en equipo, lo que me permitió desarrollar
           habilidades de colaboración y gestión de proyectos. <br></br>
           Ahora, como profesional de desarrollo <b> Full Stack </b>, mi objetivo
-          es continuar aprendiendo y creciendo en este campo tan dinámico. Estoy
-          entusiasmado por explorar nuevas tecnologías, mantenerme al día con
-          las últimas tendencias y contribuir en proyectos que tengan un impacto
-          significativo.
+          es continuar aprendiendo, actualmente me encuentro cursando una{" "}
+          <b>Tecnicatura en Desarrollo Web </b>
+          para profundizar mis conocimientos y habilidades en esta área.
         </p>
       </div>
     </section>
