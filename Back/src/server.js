33d3a2ac0135cb28498458239c9b1020986dacf4
fran-itria <francoitria01@gmail.com>
2026-03-21 00:58:45 -0300

    Try process.loadEnvFile()

--- Back/src/server.js
+++ Back/src/server.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 const transporter = require("./index");
 const { Resend } = require("resend");
 const server = express();
-process.loadEnvFile()
+
+try {
+  process.loadEnvFile()
+} catch (error) { }
+
 const PORT = process.env.PORT;
 
 const resend = new Resend(process.env.RESEND_API_KEY);
